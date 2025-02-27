53dbdc9f0fa3ed425367e6ba164c90de269e1de5
Mohammodullah Emran <63514104+emranmho@users.noreply.github.com>
2025-02-27 21:32:34 +0600

    update port

--- Movies.API/Controllers/MoviesController.cs
+++ Movies.API/Controllers/MoviesController.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
 namespace Movies.API.Controllers;
 
-public class MoviesController
+[ApiController]
+[Route("api")]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
 {
+    [HttpPost("movies")]
+    public async Task<IActionResult> CreateMovie([FromBody]CreateMovieRequest request)
+    {
+        var movie = new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
+        await movieRepository.CreateAsync(movie);
+        return Ok(movie);
+    }
     
 }
