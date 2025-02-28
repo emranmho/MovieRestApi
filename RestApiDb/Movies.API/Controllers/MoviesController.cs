01d58a8b854b954e0fa3dccc46f50bba6bcf6262
Mohammodullah Emran <63514104+emranmho@users.noreply.github.com>
2025-02-28 16:33:24 +0600

    update folder structure

---
+++ RestApiDb/Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.API.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.API.Controllers;
+
+[ApiController]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
+{
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> CreateMovie([FromBody]CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await movieRepository.CreateAsync(movie);
+        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+    }
+    
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await movieRepository.GetByIdAsync(id);
+        if(movie is null)
+        {
+            return NotFound();
+        }
+        var response = movie.MapToMovieResponse();
+        return Ok(response);
+    }
+    
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAllMovies()
+    {
+        var movies = await movieRepository.GetAllAsync();
+        var response = movies.MapToMoviesResponse();
+        return Ok(response);
+    }
+    
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> UpdateMovie([FromRoute] Guid id, 
+        [FromBody] UpdateMovieRequest request)
+    {
+        var movie = request.MapToMovie(id);
+        var updated = await movieRepository.UpdateAsync(movie);
+        if(!updated)
+        {
+            return NotFound();
+        }
+        var response = movie.MapToMovieResponse();
+        return Ok(response);
+    }
+    
+    [HttpDelete(ApiEndpoints.Movies.Delete)]
+    public async Task<IActionResult> DeleteMovie([FromRoute] Guid id)
+    {
+        var deleted = await movieRepository.DeleteByIdAsync(id);
+        if(!deleted)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
+    
+}
--- RestApiDb/Movies.API/Program.cs
+++ RestApiDb/Movies.API/Program.cs
@@ -1,17 +1,17 @@
+using Movies.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddApplicationServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    //app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
